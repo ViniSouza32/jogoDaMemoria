c3e86e9aa7503c65e5f84e7b8cf06caed4d230c9
ViniSouza32 <134454570+ViniSouza32@users.noreply.github.com>
2023-09-02 02:27:15 -0300

    Add files via upload

---
+++ jogoMemoria.js
@@ -0,0 +1,71 @@
+let words = [
+  "css3",
+  "html5",
+  "javascript",
+  "postgresql",
+  "react",
+  "typescript",
+];
+words = [...words, ...words]
+  .map((word) => {
+    return {
+      word: word,
+      random: Math.random(),
+    };
+  })
+  .sort((a, b) => a.random - b.random)
+  .map((obj) => obj.word);
+//duplica o array, ao inves de ser um array de arrays com '...' vira um so
+
+let container = document.querySelector(".container");
+let moving = [];
+
+for (let word of words) {
+  let cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+
+  let innerDiv = document.createElement("div");
+  innerDiv.classList.add("inner");
+
+  let frontDiv = document.createElement("div");
+  frontDiv.classList.add("front");
+
+  let backDiv = document.createElement("div");
+  backDiv.classList.add("back");
+
+  let image = document.createElement("img");
+  image.src = `assets/${word}.svg`;
+
+  cardDiv.appendChild(innerDiv);
+  innerDiv.appendChild(frontDiv);
+  innerDiv.appendChild(backDiv);
+  frontDiv.appendChild(image);
+
+  container.appendChild(cardDiv);
+  cardDiv.addEventListener("click", (event) => {
+    innerDiv.classList.add("flip");
+
+    moving.push(word);
+
+    if (moving.length > 1) {
+      if (moving[0] == moving[1]) {
+        document.querySelectorAll(".flip").forEach((element) => {
+          element.classList.add("right");
+          element.classList.remove("flip");
+        });
+      } else {
+        document.querySelectorAll(".flip").forEach((element) => {
+          setTimeout(() => {
+            element.parentElement.classList.add('shake');
+          setTimeout(() => {
+            element.classList.remove("flip");
+            element.parentElement.classList.remove('shake');
+          }, 500);
+          }, 500);
+        });
+      }
+
+      moving = [];
+    }
+  });
+}
